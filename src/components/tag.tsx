5c29c56d660c9fa5729ebe807405ef04f4878d54
Adil Hossain Mallick <55916006+adilmallick@users.noreply.github.com>
2022-10-22 23:48:03 +0530

    good boy

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Tag = () => {
   return (
     <div>
-      <div className="bg-gray-100 py-2 px-8 mx-1 rounded-full whitespace-nowrap cursor-pointer hover:bg-gray-300">
+      <div className="bg-gray-800 py-2 px-10 mx-1 rounded-full whitespace-nowrap cursor-pointer hover:bg-gray-800">
         Lacy Spice
       </div>
     </div>
